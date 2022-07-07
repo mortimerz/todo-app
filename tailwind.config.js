cad7601abaf83042f955687567c00e026f011e10
slarkishGuy <contact.orven@gmail.com>
2022-07-08 00:53:53 +0800

    enabled darkMode in TW

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  // added darkMode: `class` to enable toggling of dark/light
+  darkMode: 'class',
   theme: {
     extend: {},
   },
