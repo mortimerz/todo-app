ba3240d8fef7a17909d929615f2de5403a5b7c31
slarkishGuy <contact.orven@gmail.com>
2022-07-08 23:30:33 +0800

    Moved header code to the header component
    * toggling of theme was reworked to make the default theme set to dark
    * toggling of theme now will be found in Header component
    * Added title and svg icon to the header component

--- components/AppContainer.tsx
+++ components/AppContainer.tsx
@@ -1,67 +1,11 @@
-import { useEffect, useState } from "react";
+import Header from "./Header";
 
-// interface IAppContainerProps {
-//   theme: string;
-// }
 const AppContainer: React.FC = () => {
-  // set initial theme value depending on what is set in the local storage
-  const [theme, setTheme] = useState(
-    typeof window !== "undefined" ? localStorage.theme : "dark"
-  );
-
-  const colorTheme = theme === "dark" ? "light" : "dark";
-
-  useEffect(() => {
-    // this will target the root element and will add either `dark` or `light` theme
-    const root = window.document.documentElement;
-
-    root.classList.remove(colorTheme);
-    root.classList.add(theme);
-
-    if (typeof window !== "undefined") {
-      localStorage.setItem("theme", theme);
-    }
-  }, [colorTheme, theme]);
-
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <div className="container w-2/5 h-3/5">
-        {/* title */}
-        <div className="flex flex-row ">
-          <h1 className="text-4xl text-white font-bold">T O D O</h1>
-          {colorTheme === "light" ? (
-            <svg
-              onClick={() => setTheme("light")}
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                fill="#FFF"
-                fillRule="evenodd"
-                d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"
-              />
-            </svg>
-          ) : (
-            <svg
-              onClick={() => setTheme("dark")}
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                fill="#FFF"
-                fillRule="evenodd"
-                d="M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z"
-              />
-            </svg>
-          )}
-        </div>
-        <div className="border-solid border-2 border-sky-500">
+    <div className="w-screen h-screen flex flex-col justify-center items-center ">
+      <div className="app-container">
+        <Header />
+        <div className=" min-h-[250px] max-h-[500px]">
           Contents will go here
         </div>
       </div>
