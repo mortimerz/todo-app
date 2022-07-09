ed3b10a23934fd6cb9257108922100f5ebfba8c9
orven lazaro <contact.orven@gmail.com>
2022-07-10 00:42:53 +0800

    Added styling for TodoItemsContainer
    * todo options component will be added in next commits

--- components/todo/items/TodoItemsContainer.tsx
+++ components/todo/items/TodoItemsContainer.tsx
@@ -6,10 +6,29 @@ interface ITodoItemsContainer {
 }
 const TodoItemsContainer: React.FC<ITodoItemsContainer> = ({ todoItems }) => {
   return (
-    <div>
+    <div className="
+    overflow-y-auto
+    items-center 
+    flex 
+    flex-col 
+    rounded
+    max-h-[80%]
+    divide-y-[1px]
+    shadow-xl
+    dark:divide-dark-light-grayish-blue/[.20]
+    divide-light-light-grayish-blue
+    ">
       {todoItems.map((todo: ITodoItem, index) => (
         <TodoItem key={index} {...todo}></TodoItem>
       ))}
+     <div className="todo-options sticky top-[100vh]">
+      {
+        !!todoItems.length ? 
+        <div>
+          TOdo option
+        </div> : ''
+      }
+      </div>
     </div>
   )
 };
