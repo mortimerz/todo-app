5b7f3b688b36ada90141a225bb95ac02dc47382c
orven lazaro <contact.orven@gmail.com>
2022-07-09 21:31:30 +0800

    Added mock components in AppContainer
    * Added Header, Footer, TotoHolder components in the AppContainer
    * These components will be updated in the next commits

--- components/AppContainer.tsx
+++ components/AppContainer.tsx
@@ -1,16 +1,23 @@
+import Footer from "./Footer";
 import Header from "./Header";
+import TodoHolder from "./todo/TodoHolder";
+import TodoInput from "./todo/TodoInput";
 
 const AppContainer: React.FC = () => {
-  return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center ">
-      <div className="app-container">
-        <Header />
-        <div className=" min-h-[250px] max-h-[500px]">
-          Contents will go here
+    return (
+        <div className="w-screen h-screen flex flex-col justify-center items-center ">
+            <div className="app-container">
+                <Header />
+                <div className="min-h-[250px] max-h-[500px]">
+                    <TodoInput />
+                    <TodoHolder />
+                </div>
+                <Footer />
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AppContainer;
+
+
