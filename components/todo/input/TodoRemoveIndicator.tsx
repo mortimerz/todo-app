4e5be1449278fb5e8d25baacadbec6aa72ffa464
orven lazaro <contact.orven@gmail.com>
2022-07-10 20:30:17 +0800

    remove indicator mobile design

--- components/todo/input/TodoRemoveIndicator.tsx
+++ components/todo/input/TodoRemoveIndicator.tsx
@@ -6,8 +6,8 @@ interface ITodoRemoveIndicator {
 }
 const TodoRemoveIndicator: React.FC<ITodoRemoveIndicator> = ({todoStore, uuid}) => {
     return (
-        <div className="py-3 px-3 w-[10%] flex justify-center items-center" onClick={() => {todoStore.removeTodoItem(uuid)}}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" className="hidden group-hover:block">
+        <div className="py-3 px-3 flex justify-center items-center" onClick={() => {todoStore.removeTodoItem(uuid)}}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" className="block sm:hidden sm:group-hover:block">
                 <path 
                 className="fill-#494C6B dark:fill-light-dark-grayish-blue"
                     fillRule="evenodd"
