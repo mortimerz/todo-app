89732b27f5a3395d9bd6980298b629aa552a69ed
orven lazaro <contact.orven@gmail.com>
2022-07-11 10:09:14 +0800

    initial functionality for DnD
    * followed guide here: www.freecodecamp.org/news/how-to-add-drag-and-drop-in-react-with-react-beautiful-dnd/

--- components/todo/items/TodoItemsContainer.tsx
+++ components/todo/items/TodoItemsContainer.tsx
@@ -3,25 +3,55 @@ import TodoStore, { ITodoItem, TodoViewFilter } from "../../../stores/TodoStore"
 import EmptyView from "./EmptyView";
 import TodoItem from "./TodoItem";
 import TodoOptionsContainer from "./TodoOptionsContainer";
+import { DragDropContext, Droppable, Draggable, DropResult } from "react-beautiful-dnd";
+import { useState } from 'react';
 
 interface ITodoItemsContainer {
   todoStore: TodoStore;
 }
 
 const TodoItemsContainer: React.FC<ITodoItemsContainer> = ({ todoStore }) => {
+  const [todoItems, updateTodoItems] = useState(todoStore.getFilteredView());
+
+  const handleOnDragEnd = (result: DropResult) => {
+    if (!result.destination) return;
+    if (!!result) {
+      const items = Array.from(todoItems);
+      const [reorderedItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination!.index, 0, reorderedItem);
+      updateTodoItems(items);
+    }
+  }
+
   return (
     <>
       {
         todoStore.getFilteredView().length > 0 ?
           (
-            <div className="todo-items-container">
-              {todoStore.getFilteredView().map((todo: ITodoItem, index) => {
-                return (
-                  <TodoItem key={index} todoStore={todoStore} {...todo} />
-                )
-              }
-              )}
-            </div>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+              <Droppable droppableId="todoList">
+                {(provided) => (
+                  <div className="todo-items-container">
+                    <ul className="w-[100%]" id="todoList" {...provided.droppableProps} ref={provided.innerRef}>
+                      {todoStore.getFilteredView().map((todo: ITodoItem, index) => {
+                        return (
+                          <Draggable key={todo.uuid} draggableId={todo.uuid} index={index}>
+                            {(provided) => (
+                              <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                <TodoItem key={todo.uuid} todoStore={todoStore} {...todo} />
+                              </li>
+                            )}
+                          </Draggable>
+                        )
+                      }
+                      )}
+                      {provided.placeholder}
+                    </ul>
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+
           ) : (
             <EmptyView todoStore={todoStore} />
           )
